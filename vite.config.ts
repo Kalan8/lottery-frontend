28b7e04a4bd1054d8403c53dc53230125be2bd65
Thomas M <t.moesse@gmail.com>
2025-11-05 15:10:04 +1100

    Making the exposure port of the app configurable

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import path from 'node:path';
+import dotenv from 'dotenv';
+
+dotenv.config(); // load .env
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,4 +17,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+  port: Number(process.env.VITE_PORT)
+}
 })
